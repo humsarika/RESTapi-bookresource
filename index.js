008a19d126937dfec3759a1232283361ad5c7d89
Sarika Kushwaha <sarika.vsa01@gmail.com>
2022-10-07 13:46:04 +0530

    Added index.js and package.json

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const Joi = require('joi');//used for validation
+const app = express();
+app.use(express.json());
+
+const bookResource = [
+    {
+        name : 'Harry Potter and the chamber of secrets',
+        imageUrl : 'https://m.media-amazon.com/images/I/41KJfDBv7wL._SY303_BO1,204,203,200_.jpg',
+        author : 'JK Rowling',
+        pages : 1998,
+        price : '$92.47'
+    },
+    {
+        name : 'To Kill a Mockingbird',
+        imageUrl : 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/to-kill-a-mockingbird-1623750649.jpg?crop=0.908xw:1.00xh;0.0466xw,0&resize=980:*',
+        author : 'Harper Lee',
+        pages : 500,
+        price : '$100'
+    },
+    {
+        name : 'The Catcher in the Rye',
+        imageUrl : 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/catcher-in-the-rye-1623752773.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        author : 'JD Salinger',
+        pages : 600,
+        price : '$50'
+    },
+    {
+        name : 'Great Expectations',
+        imageUrl : 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/great-expectations-1623752158.jpeg?crop=1xw:1xh;center,top&resize=980:*',
+        author : 'Charles Dickens',
+        pages : 760,
+        price : '$97'
+    },
+    {
+        name : 'Lord of the Flies',
+        imageUrl : 'https://hips.hearstapps.com/harpersbazaaruk.cdnds.net/16/10/1457528919-lord-of-the-flies.jpg?resize=980:*',
+        author : 'William Golding',
+        pages : 788,
+        price : '$87'
+    },
+    {
+        name : 'The Handmaid\'s Tale',
+        imageUrl : 'https://hips.hearstapps.com/harpersbazaaruk.cdnds.net/16/10/1457529710-the-handmaids-tal.jpg?resize=980:*',
+        author : 'Margaret Atwood',
+        pages : 200,
+        price : '$56'
+    },
+    {
+        name : 'The Scarlet Letter',
+        imageUrl : 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-scarlet-letter-1623752522.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        author : 'Nathaniel Hawthorne',
+        pages : 380,
+        price : '$76'
+    },
+    {
+        name : 'Lolita',
+        imageUrl : 'https://hips.hearstapps.com/harpersbazaaruk.cdnds.net/16/10/1457528777-lolita.jpg?resize=980:*',
+        author : 'Vladimir Nobokov',
+        pages : 290,
+        price : '$60'
+    }
+]
